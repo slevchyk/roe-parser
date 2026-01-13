ed6e5346186b6c8a51a7c013682532a57ccb49e0
slevchyk <s.levchyk@gmail.com>
2026-01-13 16:55:17 +0200

    added date to cron log

--- update_calendar.sh
+++ update_calendar.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Переходимо в папку з проектом
-cd /root/roe-parser
+cd /root/roe-parser || exit
+
+echo "[$(date +'%d.%m %H:%M')] --- Запуск сесії ---"
 
 # Запускаємо парсер
 ./roe-parser
 
-#Додаємо файлів з теки data
+# Додаємо файли, якщо є зміни
 if [[ -n $(git status -s data/) ]]; then
-    echo "Оновлення графіків у теці data..."
+    echo "[$(date +'%H:%M')] Виявлено зміни. Відправка на GitHub..."
     git add data/discos-*.ics
     git commit -m "Auto-update: $(date +'%d.%m.%Y %H:%M')"
     git push origin main
-    echo "Всі файли відправлено на GitHub!"
+    echo "[$(date +'%H:%M')] GitHub оновлено успішно."
 else
-    echo "Змін у теці data не знайдено."
+    echo "[$(date +'%H:%M')] Змін у теці data немає."
 fi
