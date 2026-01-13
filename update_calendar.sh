a44bb4c2f00328f6d4560ce6498b1298d55205e1
slevchyk <slevchyk@example.com>
2026-01-13 12:46:51 +0200

    update calendar script added

---
+++ update_calendar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Переходимо в папку з проектом
+cd /root/roe-parser
+
+# Запускаємо парсер
+./roe-parser
+
+#Додаємо файлів з теки data
+if [[ -n $(git status -s data/) ]]; then
+    echo "Оновлення графіків у теці data..."
+    git add data/discos-*.ics
+    git commit -m "Auto-update all groups in data folder: $(date +'%d.%m.%Y %H:%M')"
+    git push origin main
+    echo "Всі файли відправлено на GitHub!"
+else
+    echo "Змін у теці data не знайдено."
+fi
